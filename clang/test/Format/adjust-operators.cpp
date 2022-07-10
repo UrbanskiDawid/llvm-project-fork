4c08dde5ceefa8faf68a61a74c541aa09608c29c
Dawid Urbański <git@dawidurbanski.pl>
2022-07-10 23:40:17 +0200

    fix: adjust-operators

--- clang/test/Format/adjust-operators.cpp
+++ clang/test/Format/adjust-operators.cpp
@@ -1,14 +1,15 @@
 // RUN: grep -Ev "// *[A-Z-]+:" %s \
-// RUN:   | clang-format -style="{SpaceBeforeAndAfterOperator:SpaceBeforeAndAfter}" -lines=1:2 \
-// RUN:   | FileCheck -strict-whitespace %s
+// RUN:   | clang-format -style="{SpaceBeforeAndAfterOperator: SpaceBeforeAndAfter}" -lines=1:2 \
+// RUN:   | FileCheck -strict-whitespace %s --check-prefixes=CHECKON
+// CHECKON: {{^bool is = \(1 == 1\);}}
+// CHECKON: {{^int a = 2 \+= 2;}}
 bool is=(1==1);
-int a = 1+=1;
-// CHECK: {{^bool is=\(1\ ==\ 1\);}}
-// CHECK: {{^int a=1 + 1;}}
-// RUN:   | clang-format -style="{SpaceBeforeAndAfterOperator:None}" -lines=3:4 \
-// RUN:   | FileCheck -strict-whitespace %s
-bool is2=(1==1);
-int a2 = 1+=1;
-// CHECK: {{^bool is2=\(1==1\);}}
-// CHECK: {{^int a2=1+1;}}
+int a=2+=2;
+// RUN: grep -Ev "// *[A-Z-]+:" %s \
+// RUN:   | clang-format -style="{SpaceBeforeAndAfterOperator: None}" -lines=3:4 \
+// RUN:   | FileCheck -strict-whitespace %s --check-prefixes=CHECKOFF
+// CHECKOFF: {{^bool is3 = 3==3}}
+// CHECKOFF: {{^int a4 = 4\+=4;}}
+bool is3=3==3;
+int a4=4+=4;
 
